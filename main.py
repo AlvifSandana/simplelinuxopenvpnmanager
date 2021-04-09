6b8de0d82e0774dda64da22cf1e1fd025e87ec18
alvif <asm240800@gmail.com>
2021-04-09 15:39:09 +0700

    add get public ip address function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import eel
 import os
 import platform
+import requests
 import subprocess
 
 # initiate eel
@@ -17,10 +18,11 @@ def getsysinfo():
 @eel.expose
 def getovpnstatus():
     check = subprocess.Popen(["which", "openvpn"], stdout=subprocess.PIPE, text=True)
+    result = check.communicate()[0]
     if check.poll() != 0:
         message = 'Not Installed'
     else:
-        message = 'Installed'
+        message = f'Installed ({result})'
     return message
 
 
@@ -31,7 +33,7 @@ def getovpnlistfiles():
         conf_dir = os.getcwd() + '/config_dir'
         list_proc = subprocess.Popen(["find", f"{conf_dir}", "-iname", "*.ovpn"], stdout=subprocess.PIPE, text=True)
         stdout = list_proc.communicate()[0]
-        list_conf = stdout.split("/home/alvif/PycharmProjects/linuxopenvpnmanager/config_dir/")
+        list_conf = stdout.split('/home/alvif/PycharmProjects/linuxopenvpnmanager/config_dir/')
     except Exception as error:
         print(error)
     return list_conf
@@ -39,21 +41,36 @@ def getovpnlistfiles():
 
 @eel.expose
 def connectvpn(config_name):
-    global conn_msg
+    # global conn_msg
+    # try:
+    #     conf_dir = os.getcwd() + '/config_dir/' + config_name
+    #     current_ip = str(os.system("curl https://ipinfo.io/ip"))
+    #     conn_proc = subprocess.Popen(["sudo", "openvpn", "--config", f"'{conf_dir}'"],
+    #                                  stdout=subprocess.PIPE, text=True)
+    #     while True:
+    #         check_ip = str(os.system("curl https://ipinfo.io/ip"))
+    #         if current_ip != check_ip:
+    #             conn_msg = 'Connected'
+    #         else:
+    #             conn_msg = 'Not Connected'
+    # except Exception as error:
+    #     print(error)
+    # return conn_msg
+    pass
+
+
+@eel.expose
+def checkpublicip():
     try:
-        conf_dir = os.getcwd() + '/config_dir/' + config_name
-        current_ip = str(os.system("curl https://ipinfo.io/ip"))
-        conn_proc = subprocess.Popen(["sudo", "openvpn", "--config", f"'{conf_dir}'"],
-                                     stdout=subprocess.PIPE, text=True)
-        while True:
-            check_ip = str(os.system("curl https://ipinfo.io/ip"))
-            if current_ip != check_ip:
-                conn_msg = 'Connected'
-            else:
-                conn_msg = 'Not Connected'
+        url = 'https://ipinfo.io/ip'
+        request = requests.get(url)
+        ip_addr = request.text
+        if ip_addr != '' or ip_addr is not None:
+            return ip_addr
+        else:
+            return '127.0.0.1'
     except Exception as error:
         print(error)
-    return conn_msg
 
 
 # run
--- tes.py
+++ tes.py
@@ -1,27 +1,41 @@
-import os
-import subprocess
+# import os
+# import subprocess
+#
+#
+# def connectVPN(config_name):
+#    try:
+#       conf_dir = f"{os.getcwd()}/config_dir"
+#       filename = f"'{config_name}'"
+#       current_ip = os.system("curl https://ipinfo.io/ip")
+#       print(conf_dir)
+#       # passku = '240800'
+#       # subprocess.Popen(["echo", passku], stdout=subprocess.PIPE)
+#       # subprocess.Popen(["sudo", "openvpn", "--config", filename],cwd=conf_dir).communicate(input=b'240800\n')
+#       os.system(f"sudo openvpn --config {filename}")
+#       #   while True:
+#       #       check_ip = str(os.system("curl https://ipinfo.io/ip"))
+#       #       if current_ip != check_ip:
+#       #           conn_msg = 'Connected'
+#       #       else:
+#       #           conn_msg = 'Not Connected'
+#    except Exception as error:
+#       print(error)
+#
+# # connectVPN("server5-VPNSplit.com.ovpn")
+#
+# proc = subprocess.Popen(["sudo", "openvpn3" ,"session-start", "--config", "/home/alvif/PycharmProjects/linuxopenvpnmanager/config_dir/server5-VPNSplit.com.ovpn"], stdout=subprocess.PIPE, text=True)
+# # proc.communicate(input=b'vpnsplit.com-100470492502109\n240800')
+import requests
 
 
-def connectVPN(config_name):
-   try:
-      conf_dir = f"{os.getcwd()}/config_dir"
-      filename = f"'{config_name}'"
-      current_ip = os.system("curl https://ipinfo.io/ip")
-      print(conf_dir)
-      # passku = '240800'
-      # subprocess.Popen(["echo", passku], stdout=subprocess.PIPE)
-      # subprocess.Popen(["sudo", "openvpn", "--config", filename],cwd=conf_dir).communicate(input=b'240800\n')
-      os.system(f"sudo openvpn --config {filename}")
-      #   while True:
-      #       check_ip = str(os.system("curl https://ipinfo.io/ip"))
-      #       if current_ip != check_ip:
-      #           conn_msg = 'Connected'
-      #       else:
-      #           conn_msg = 'Not Connected'
-   except Exception as error:
-      print(error)
+def checkpublicip():
+    try:
+        url = 'https://ipinfo.io/ip'
+        request = requests.get(url)
+        ip_addr = request.text
+        print(ip_addr)
+    except Exception as error:
+        print(error)
 
-# connectVPN("server5-VPNSplit.com.ovpn")
 
-proc = subprocess.Popen(["sudo", "openvpn3" ,"session-start", "--config", "/home/alvif/PycharmProjects/linuxopenvpnmanager/config_dir/server5-VPNSplit.com.ovpn"], stdout=subprocess.PIPE, text=True)
-# proc.communicate(input=b'vpnsplit.com-100470492502109\n240800')
+checkpublicip()
